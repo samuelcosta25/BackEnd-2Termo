936c3b23fff8fb5992521848745a48d07c3974c0
samuelcosta25 <samu.live@outlook.com>
2025-08-01 16:48:45 -0300

    01.08.25

---
+++ Aula 01/variavies.php
@@ -0,0 +1,12 @@
+<?php
+/*Desenvolva um código com a mesma estrutura que o
+abaixo, porem com os seus dados.*/
+
+    echo"Olá! \n";
+    $nome ="Samuel! \n";
+    $idade ="25";
+    $ano_atual = "2025";
+
+    $data_nasc= $ano_atual-$idade;
+    echo $nome, "você nasceu em:", $data_nasc;
+?>
