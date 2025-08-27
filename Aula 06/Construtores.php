6e7181007ae4f8fa018de1dc91f3d3198a0bf2f4
samuelcosta25 <samu.live@outlook.com>
2025-08-27 13:18:58 -0300

    27.08.25

--- Aula 06/Construtores.php
+++ Aula 06/Construtores.php
@@ -1,39 +1,40 @@
 <?php
-class Produtos{
+class ProdutosMercado{ // Criando classe
     // Atributos
     public $nome;
     public $categoria;
     public $fornecedor;
     public $qtde_estoque;
 
-    // Criando construtor
-    public function __construct($nome, $categoria, $fornecedor, $qtde_estoque){
-        $this->nome = $nome;
-        $this->categoria = $categoria;
-        $this->fornecedor = $fornecedor;
-        $this->qtde_estoque = $qtde_estoque;
-    }
-// Métodos
-    public function produto_vendido($qtde_vendida){
+    // Métodos
+    public function atualizarEstoque($qtde_vendida){
         $this->qtde_estoque -= $qtde_vendida;
         return $this->qtde_estoque;
     }
+
+    // Criando construtor
+    public function __construct($nome, $categoria, $fornecedor, $qtde_estoque){
+        $this-> nome = $nome;
+        $this-> categoria = $categoria;
+        $this-> fornecedor= $fornecedor;
+        $this-> qtde_estoque = $qtde_estoque;
+    }
 }
 
-// Criando objetos sem a existencia de um construtor
-$bolacha1 = new Produtos();
-$bolacha1 -> nome = "Nikito";
-$bolacha1 -> categoria = "Doces";
-$bolacha1 -> fornecedor = "Vitarella";
-$bolacha1 -> qtde_estoque = 220;
+// Criando objetos sem construtor feito
 
-$feijao1 = new Produtos();
-$feijao1 -> nome = "Oliron";
-$feijao1 -> categoria = "Mantimentos";
-$feijao1 -> fornecedor = "Reserva nobre";
-$feijao1 -> qtde_estoque = 123;
+// $produto1 = new ProdutosMercado();
+// $produto1 -> nome = "Suco Tang";
+// $produto1 -> categoria = "Bebidas";
+// $produto1 -> fornecedor = "Mondelez";
+// $produto1 -> qtde_estoque = 200; 
 
-// Criando objetos com a existencia de um construtor
-$bolacha1 = new Produtos("Nikito", "Doces", "Vitarella", 220);
+// $produto2 = new ProdutosMercado();
+// $produto2 -> nome = "Energético Monster";
+// $produto2 -> categoria = "Bebidas";
+// $produto2 -> fornecedor = "Coca-Cola";
+// $produto2 -> qtde_estoque = 150; 
 
-$feijao1 = new Produtos("Oliron", "Mantimentos", "Reserva nobre", 123);
+// Criando objetos com construtor feito
+$produto1 = new ProdutosMercado ("Suco Tang", "Bebidas", "Mondelez", 200);
+$produto2 = new ProdutosMercado ("Energético Monster", "Bebidas", "Coca-Cola", 150);
--- Aula 06/Exercicio.php
+++ Aula 06/Exercicio.php
@@ -1,12 +1,22 @@
 <?php
-// Crie uma classe "Produtos" com ao menos 4 atributos sem a utilização de um construtor
+// Crie uma classe "Moto" com ao menos 4 atributos sem a utilização de um construtor
 
-class Produtos{
+class Moto{
 
 // [...]
 
 }
 
+
 // Crie ao menos 3 objetos para essa classe
-$objeto1 = new Produtos();
+$objeto1 = new Moto();
 // [...]
+
+// Crie 3 construtores sendo:
+// 1° Construtor: Recebe 3 parâmetros sendo eles $dia, $mes e $ano;
+
+// 2° Construtor: Recebe 7 parâmetros sendo eles: $nome, $idade, $cpf, $telefone, $endereco, $estado_civil e $sexo;
+
+// 3° Construtor: Recebe 5 parâmetros sendo eles: $marca, $nome, $categoria, $data_fabricacao e $data_venda;
+
+// OBS: Escreva o exercício em forma de comentário.
