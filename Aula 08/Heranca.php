30c18cd2d390979f3f40829669ee72decf3ba9d4
samuelcosta25 <samu.live@outlook.com>
2025-08-29 16:49:18 -0300

    29.08.25

---
+++ Aula 08/Heranca.php
@@ -0,0 +1,37 @@
+<?php
+ class Imovel {
+    private $categoria;
+    private $n_comodos;
+    private $valor;
+    private $estado_conservacao;
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao){
+        $this->$categoria=$categoria;
+        $this->n_comodos=$n_comodos;
+        $this->valor-$valor;
+        $this->estado_conservacao=$estado_conservacao;
+    }
+ }
+
+ class Casa extends Imovel{
+    private $tem_quintal; // Atributo booleano - true ou false
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao, $tem_quintal){
+        parent::__construct($categoria, $n_comodos, $valor, $estado_conservacao);
+
+        $this->tem_quintal=$tem_quintal;
+    }
+ }
+
+ class Apartamento extends Imovel{
+    private $andar;
+
+    public function __construct($categoria, $n_comodos, $valor, $estado_conservacao, $andar){
+        parent::__construct($categoria, $n_comodos, $valor, $estado_conservacao);
+
+        $this->andar=$andar;
+    }
+ }
+
+ // Crie uma subclasse chamada Escola com o atributo $seguimento.
+  
