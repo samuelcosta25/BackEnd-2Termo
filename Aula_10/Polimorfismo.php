0b84d15f4f8ac310889b5aff2c8dd03a95eb59a2
samuelcosta25 <samu.live@outlook.com>
2025-09-17 16:56:20 -0300

    17.09.25

--- Aula_10/Polimorfismo.php
+++ Aula_10/Polimorfismo.php
@@ -1,14 +1,31 @@
 <?php
+namespace Aula_10;
 // Polimorfismo:
-
-// O termo Polimorfismo significa "várias formas". Associando isso a Programação Orientada a Objetos,  o conceito se trata de várias classes e suas instâncias (objetos) respondendo a um mesmo método de formas diferentes. No exemplo do Interface da Aula_09, temos um método CalcularArea() que responde de forma diferente à classe Quadrado, à classe Pentágono e a classe Círculo. Isso quer dizer que a função é a mesma - calcular a área da forma geométrica - mas a operação muda de acordo com a figura. 
+// O termo Polimorfismo significa "várias formas". Associando isso a Programação Orientada a Objetos,  o conceito se trata de várias classes e suas instâncias (objetos) respondendo a um mesmo método de formas diferentes. No exemplo do Interface da Aula_09, temos um método CalcularArea() que responde de forma diferente à classe Quadrado, à classe Pentágono e à classe Círculo. Isso quer dizer que a função é a mesma - calcular a área da forma geométrica - mas a operação muda de acordo com a figura. 
 
 // Crie um método chamado "mover()", aonde ele responde de varias formas diferentes, para as sub-classes: Carro, Avião, Barco e Elevador. Dica: utilize Interfaces.
 
 interface Veiculo {
-    public function mover(){}
+    public function mover();
+}
+
+class Carro implements Veiculo{
+    public $nome;
+    public function mover(){
+        echo "O Carro {$this->nome} está andando";
+    }
+}
+
+class Aviao implements Veiculo{
+    public $nome;
+     public function mover(){
+        echo "O avião {$this->nome} está voando";
+    }
 }
 
 // ===================================================
-// Crie ao menos 2 objetos para testar cada classe =D
+// Crie ao menos 2 objetos para testar CADA classe =D
 // ===================================================
+
+$carro1 = new Carro ();
+$carro1->nome = "Civic";
